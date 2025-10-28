de50cae3485d8c27988f102247e16abb90816b87
Pakgad95 <s6752410029@sau.ac.th>
2025-10-28 16:21:35 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, BrowserRouter } from "react-router-dom";
+import Home from "./page/Home";
+import All from "./page/ShowAllKinkun";
+import Add from "./page/Addkinkun";
+import Edit from "./page/Editkinkun";
+
+export default function App() {
+  return (
+    <>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={Home} />
+        <Route path="/Add" element={Add} />
+        <Route path="/Edit" element={Edit} />
+        <Route path="/All" element={All} />
+      </Routes>
+    </BrowserRouter>
+    
+    </>
+  )
+}
